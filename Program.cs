2125ced8cb91a461ca0faaba7757a8c0ffc3e5d5
Denis Zorich <zorichd@gmail.com>
2022-09-15 22:04:25 +0300

    Семенар C#

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿// Console.WriteLine("Ведите первое число");
+// int numberA = int.Parse(Console.ReadLine());
+// Console.WriteLine("Ведите второе число");
+// int numberB = int.Parse(Console.ReadLine());
+
+// if(numberA/numberB == numberB)
+// Console.WriteLine("Да");
+// else
+// Console.WriteLine("Нет");
+
+// Console.WriteLine("Чтобы узнать день недели введите число от 1 до 7))");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (firstNumber == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (firstNumber == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (firstNumber == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (firstNumber == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (firstNumber == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (firstNumber == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("просил число от 1 до 7");
+// }
+
+// Console.WriteLine("Введите число:");
+// int number = int.Parse(Console.ReadLine());
+
+// switch (number)
+// {
+//     case 1:
+//         Console.WriteLine("ПН");
+//         break;
+//     case 2:
+//         Console.WriteLine("ВТ");
+//         break;
+//     case 3:
+//         Console.WriteLine("СР");
+//         break;
+//     case 4:
+//         Console.WriteLine("ЧТ");
+//         break;
+//     case 5:
+//         Console.WriteLine("ПТ");
+//         break;
+//     case 6:
+//         Console.WriteLine("СБ");
+//         break;
+//     case 7:
+//         Console.WriteLine("ВС");
+//         break;
+//     default:
+//         Console.WriteLine("Ввели чушь");
+//         break;
+
+// }
+
+// Console.WriteLine("Ведите число");
+// int number = int.Parse(Console.ReadLine());
+
+// for(int i = -number; i <= number; i++)
+// Console.Write($"{i} ");
+
+// Console.WriteLine("Введите трехзначное число");
+// int number = int.Parse(Console.ReadLine());
+
+// if (number > 99 && number < 1000)
+// Console.WriteLine(number % 10);
+//     else
+//     Console.WriteLine("Ошибка, число не трёхзначное");
+   
